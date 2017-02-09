32629cca43967a28b3200dee629441cd40d7cc7d
Adriano Waltrick <akuma.majunia@gmail.com>
2017-02-09 16:04:01 -0200

    Testes iniciais com electron. Menu e Janela

---
+++ photon/dist/template-app/js/menu.js
@@ -0,0 +1,43 @@
+var remote = require('electron').remote;
+var Menu = remote.Menu;
+
+// recupera a função la do main
+const main = remote.require('./main.js');
+
+const menu = Menu.buildFromTemplate(
+    [
+        {
+            label: 'Meu Menu',
+            submenu: [
+                {
+                    label: 'Mensagem no Log',
+                    click () {
+                        console.log('Mensagem!!!!');
+                        ipcRenderer.send('asynchronous-message', 'minha mensagem que venho do render');
+                    }
+                },
+                {
+                    label: 'Abrir Janela',
+                    click () {
+                        main.openNewWindow();
+                    }
+                },
+
+            ]
+        }
+    ]
+)
+
+Menu.setApplicationMenu(menu);
+
+
+/**
+ * Mensagem do render para o main
+ */
+// In renderer process (web page).
+const {ipcRenderer} = require('electron')
+console.log(ipcRenderer.sendSync('synchronous-message', 'ping')) // prints "pong"
+
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+  console.log('from render: ' + arg) // prints "pong"
+})
